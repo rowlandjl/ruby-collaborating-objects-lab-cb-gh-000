b1adac4ae12c377a1d2da4141141a95934791a1f
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 14:42:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,9 @@ class Song
     @name = name
   end
 
-  def new_by_filename
+  def new_by_filename(filename)
+    parse = filename.split(" - ")
+    artist = Artist.find_or_create_by_name(parse[0])
   end
 
 end
