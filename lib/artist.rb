779080803259cded578e05742edda6cc799abdb1
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 14:37:35 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+
+  @@all = []
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(name)
+    @songs << name
+  end
+
+  def find_or_create_by_name(name)
+  end
+
+end
